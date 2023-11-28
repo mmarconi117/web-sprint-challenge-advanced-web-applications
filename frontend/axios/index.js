69ebcac1335af6d64c4edd39cbbe4087c5d23371
Michael Marconi <michaelziga2@gmail.com>
2023-11-28 10:47:07 -0600

    401

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,14 +1,11 @@
-import axios from 'axios'
-
-
+import axios from 'axios';
 
 const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
 
   const instance = axios.create({
-    baseURL: 'http://localhost:9000/api',
+    baseURL: 'http://localhost:9000/api/',
     headers: {
-      'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
   });
@@ -19,4 +16,4 @@ const axiosWithAuth = () => {
   return instance;
 };
 
-export default axiosWithAuth
+export default axiosWithAuth;
